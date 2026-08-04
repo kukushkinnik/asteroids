719b8ac02772bf81cb5097827eb0e45c472cb9da
Nikita Kukushkin <kukushkinnika@gmail.com>
2026-08-03 23:18:29 -0400

    delta time calculation

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0 
     while True:
         log_state()
 
@@ -12,6 +14,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        dt = clock.tick(60) / 1000
         screen.fill("black")
         pygame.display.flip()
 
